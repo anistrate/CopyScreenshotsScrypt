ffcacb255ce98d1739f0a987f625278efff32893
anistrate <istrandrei@gmail.com>
2021-03-26 21:58:39 +0200

    feat: added initial files
    feat: added initial files

---
+++ get process handles.ps1
@@ -0,0 +1,17 @@
+﻿$Processes = Get-Process mpc-hc
+$results = $Processes | Foreach-Object {
+    $handles = (handle64 -p $_.ID -NoBanner) | Where-Object { $_ -Match " File " } | Foreach-Object {
+            [PSCustomObject]@{
+        "Hex"  = ((($_ -Split " ").Where({ $_ -NE "" })[0]).Split(":")[0]).Trim()
+        "File" = (($_ -Split " ")[-1]).Trim()
+        }
+    }
+
+    If ( $handles ) {
+        [PSCustomObject]@{
+            "Name"    = $_.Name
+            "PID"     = $_.ID
+            "Handles" = $handles
+        }
+    }
+}
---
+++ screenshot moving.ps1
@@ -0,0 +1,15 @@
+﻿    $watcher = New-Object System.IO.FileSystemWatcher
+    $watcher.Path = "D:\Things that can fuck off\test"
+    $watcher.Filter = "*.*"
+    $watcher.IncludeSubdirectories = $true
+    $watcher.EnableRaisingEvents = $true 
+
+    $action = { $path = $Event.SourceEventArgs.FullPath
+                $changeType = $Event.SourceEventArgs.ChangeType
+                $logline = "$(Get-Date), $changeType, $path"
+                Add-content "D:\log.txt" -value $logline
+              }
+
+    Register-ObjectEvent $watcher "Created" -Action $action
+
+    while ($true) {sleep 5}
